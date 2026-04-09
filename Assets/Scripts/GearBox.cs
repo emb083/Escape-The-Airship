f0aa30af185df1afddef6c577810e6dd0f415894
Voldermen <153773095+Voldermen@users.noreply.github.com>
2026-04-08 20:25:10 -0500

    Finished gear box animations
    I finished the gear box animations and added a new gear prefab next push will have a working exit door with animations

--- Assets/Scripts/GearBox.cs
+++ Assets/Scripts/GearBox.cs
@@ -1,11 +1,27 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
 
 public class GearBox : MonoBehaviour
 {
     public GameObject ExitDoor;
+    public GameObject GearLeft, GearRight, GearMiddle;
+
    private int gearsNeeded=3;
    private bool isDoorOpen=false;
 
+   public Animator counterClock;
+   public Animator clockwiseLeft;
+   public Animator clockwiseRight;
+
+ private void Start()
+    {
+        
+        GearLeft.SetActive(false);
+        GearMiddle.SetActive(false);
+        GearRight.SetActive(false);
+    }
    void Grab()
     {
         if (isDoorOpen) // safty check to make sure the door opens once and gears are not removed again.
@@ -15,16 +31,23 @@ public class GearBox : MonoBehaviour
         if (Inventory.Instance.NumberOfGears() >= gearsNeeded) // if the right number of gears is in the player inventory when they interact with the gear box the gears are removed and the door opens.
         {
             Inventory.Instance.RemoveGear(gearsNeeded);
-            doorOpen();
+            DoorOpen();
         } else
         {
             Debug.Log("I need more gears to get this working.");
         }
     }
 
-    void doorOpen()
+    void DoorOpen()
     {
        isDoorOpen=true;
+        
+        GearLeft.SetActive(true);
+        GearMiddle.SetActive(true);
+        GearRight.SetActive(true);
+        counterClock.SetTrigger("gearRotation");
+        clockwiseRight.SetTrigger("gearTurn");
+        clockwiseLeft.SetTrigger("gearTurn");
        ExitDoor.SetActive(false); // add door open animation here. 
     }
 }
