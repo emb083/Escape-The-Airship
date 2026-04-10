01165a12b9ff80ac174a008975b00fea8639b0a6
Voldermen <153773095+Voldermen@users.noreply.github.com>
2026-04-09 22:29:34 -0500

    gear animation added and bug fixes
    A gear falling was added when the door opens.

--- Assets/Scripts/ConnectionPuzzle.cs
+++ Assets/Scripts/ConnectionPuzzle.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class ConnectionPuzzle : MonoBehaviour {
   // set in inspector
   public List<Gear> gears;
-
+  public GameObject fallingGear;
+  public Animator gearDrop;
   // private fields
   private Animator roomDoorAnimator;
   private int[] code;
 
   private void Start() {
+    fallingGear.SetActive(false);
     roomDoorAnimator = GetComponent<Animator>();
     code = new[] { 3, 4, 6, 1 };
   }
@@ -23,6 +25,8 @@ public class ConnectionPuzzle : MonoBehaviour {
     if (solved) {
       gears.ForEach(gear => gear.ChangeState(GearState.DISABLED));
       roomDoorAnimator.SetTrigger("Door Open");
+      fallingGear.SetActive(true);
+      gearDrop.SetTrigger("falling");
     }
   }
 }
