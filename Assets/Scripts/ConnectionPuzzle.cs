180ae6c55b10a784d9527d48f21db9330d43cc49
Voldermen <153773095+Voldermen@users.noreply.github.com>
2026-04-10 00:40:06 -0500

    more bug fixes second gear animation added

--- Assets/Scripts/ConnectionPuzzle.cs
+++ Assets/Scripts/ConnectionPuzzle.cs
@@ -5,27 +5,34 @@ using UnityEngine;
 public class ConnectionPuzzle : MonoBehaviour {
   // set in inspector
   public List<Conductor> gears;
-  public List<ParticleSystem> sparks;
+  public GameObject keyGear;
+  public Animator gearFalling;
 
   // private fields
   private Animator roomDoorAnimator;
-
+  private bool isSolved= false;
   private void Start() {
+    keyGear.SetActive(false);
     roomDoorAnimator = GetComponent<Animator>();
   }
 
   private void Update() {
-    bool solved = true;
+    if (isSolved)
+    {
+      return;
+    }
+    bool solved=true;
     for (int i = 0; i < gears.Count; i++) {
       solved &= gears[i].GetConnection();
     }
     if (solved) {
-      for (int i=0; i<sparks.Count; i++){
-        sparks[i].Play();
-      }
+      isSolved=true;
       SoundManager.Play(SoundType.ZAP);
       gears.ForEach(gear => gear.ChangeState(ConductorState.DISABLED));
       roomDoorAnimator.SetTrigger("Door Open");
+      keyGear.SetActive(true);
+      gearFalling.SetTrigger("falling");
+      SoundManager.Play(SoundType.FALL);
       SoundManager.Play(SoundType.DOOR);
     }
   }
